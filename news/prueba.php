9165f8411463c931cd76292f74a1cf88bb5e267b
Liferbis <liferbis@gmail.com>
2014-10-29 18:57:47 +0100

    prueba V1
    version1

---
+++ news/prueba.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Grupo Codelse</title>
+		<!-- Bootstrap CSS -->
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">
+					<h1 class="text-center"><strong>Grupo Codelse</strong></h1>
+					<!--<img class="img-responsive" src="img/Logo.jpg"/>-->
+				</div>
+			</div>
+			<div class="row well">
+				<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 ">
+					<img class="imagen-responsive" src="img/code1.jpg"/></br>
+				</div>
+				<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 ">		
+					<h3><strong>CODELSE es una empresa líder en el mercado de las instalaciones especiales con más de 30 años de experiencia en el sector.</strong></h3>
+					<p></p>	
+					Diseñamos, instalamos y mantenemos todo tipo de sistemas de seguridad , telecomunicaciones, informática, TIC, climatización, electricidad, diseño, fabricación y montaje de museografías, certificación energética y análisis de patologías estructurales de edificios... 
+					<p></p>
+					<p></p>
+					<a href="http://www.codelse.com" target="_black"><button type="button" class="btn btn-large btn-block btn-success"><span class="glyphicon glyphicon-eye-open"/> Seguir leyendo</button></a>
+				</div>
+			</div>
+			<div class="row well">
+				<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3 ">
+					<img class="imagen-responsive" src="img/centro-de-adultos-torrelavega.jpg"/></br>
+				</div>
+				<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9 ">		
+					<h3><strong>Las obras del Centro de Adultos del Urban avanzan a buen ritmo y acabarán este año</strong></h3>
+					<p></p>	
+					<p class="text-justify">Las obras de construcción del Centro de Adultos y Ludoteca en la Inmobiliaria, uno de los principales proyectos del Plan Urban para Torrelavega, que supone una inversión de millones de euros, avanzan a buen ritmo y estarán finalizadas este año...</p>
+					<p></p>
+					<p></p>
+					<a href="http://www.gentedigital.es/santander/noticia/1397024/las-obras-del-centro-de-adultos-del-urban-avanzan-a-buen-ritmo-y-acabaran-este-ano/" target="_black"><button type="button" class="btn btn-large btn-block btn-success" > <span class="glyphicon glyphicon-eye-open"/> Seguir leyendo</button></a>
+				</div>
+			</div>
+
+			<div class="row well">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 ">		
+					<a href="https://www.google.es/maps/place/GRUPO+CODELSE/@43.328101,-4.037144,17z/data=!3m1!4b1!4m2!3m1!1s0xd493e6a15838ee3:0x8ea066788016a1ae" target="_black"><button type="button" class="btn btn-large btn-block btn-success"> <span class="glyphicon glyphicon-map-marker"/> Poligono Industrial Tanos-Viernoles Parcela B-4</button></a>
+				</div>
+			</div>
+
+			<div class="row well">
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 ">
+					<button type="button" class="btn btn-success" >
+						<a href="https://es-es.facebook.com/pages/Grupo-Codelse/220932171280815" target="_black">
+							<img class="imagen-responsive" src="img/redes/FaceBook.png" alt="facebook" />
+						</a>
+					</button>
+				</div>
+
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 ">
+					<button type="button" class="btn btn-success" >
+						<a href="https://twitter.com/GrupoCodelse" target="_black">
+							<img class="imagen-responsive" src="img/redes/Twitter.png" alt="twitter" />
+						</a>
+					</button>
+				</div>
+
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 ">
+					<button type="button" class="btn btn-success" >
+						<a href="http://www.linkedin.com/company/grupo-codelse" target="_black">
+							<img class="imagen-responsive" src="img/redes/Linkedin.png" alt="linkedin" />
+						</a>
+					</button>
+				</div>
+
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 ">
+					<button type="button" class="btn btn-success" >
+						<a href="https://www.youtube.com/user/codelse" target="_black">
+							<img class="imagen-responsive" src="img/redes/youtube.png" alt="youtube" />
+						</a>
+					</button>
+				</div>
+
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 ">
+					<button type="button" class="btn btn-success" >
+						<a href="http://vimeo.com/channels/codelse" target="_black">
+							<img class="imagen-responsive" src="img/redes/Vimeo.png" alt="Vimeo" />
+						</a>
+					</button>
+				</div>
+
+				
+			</div>
+
+			<div class="pie">
+				<div class="row well">
+
+					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">
+						<a href="tel:+34942804480">
+							<button type="button" class="btn btn-success" >
+								<span class="glyphicon glyphicon-phone-alt"/> 
+								942.804.480
+							</button>
+						</a>
+					</div>
+
+					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">
+						<a "http://www.codelse.com" target="_black">
+							<button type="button" class="btn btn-success" >
+								<span class="glyphicon glyphicon-home"/> 
+								GRUPO CODELSE
+							</button>
+						</a>
+					</div>
+
+					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">
+						<a href="mailto:codelse@codelse.com">
+							<button type="button" class="btn btn-success" >
+								<span class="glyphicon glyphicon-envelope" alt="Correo" />
+								Codelse@codelse.com
+							</button>
+						</a>
+					</div>
+
+					<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 ">
+						<img class="imagen-responsive" src="img/Aenor.png" alt="Aenor" />
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	</body>
+</html>
