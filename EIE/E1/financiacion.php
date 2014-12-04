1ed80ef4ceff82a768ead189447c01035f71ab0f
Liferbis <liferbis@gmail.com>
2014-12-04 20:13:14 +0100

    eie

--- EIE/E1/financiacion.php
+++ EIE/E1/financiacion.php
@@ -156,7 +156,8 @@
 											guardarlo sin aplicación alguna
 										</li>
 										<li>
-											incluirlo en las reservas 
+											incluirlo en las reservas 
+
 										</li>
 										<li>
 											reinvertirlo en la empresa
@@ -288,23 +289,46 @@
 					<ul>
 						<li class="list-group-item">
 							<strong>
-								1.	Emisiones de obligaciones y bonos 
+								1.	Emisiones de obligaciones y bonos
 							</strong>
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Son los llamados valores de renta fija. Son títulos que se venden a los ahorradores a un precio determinado y que pasado un tiempo hay que recomprar dichos títulos a un precio que puede ser igual o superior que el valor que los ahorradores pagaron por ellos. Es pues una especie de préstamo que le hacen a la empresa durante un periodo de tiempo determinado, y que esta tiene que devolver de una sola vez. Además también se pueden pagar intereses anuales por ellos durante el tiempo que dure la vida de los títulos
+								</li>
+								<li>
+									Para las empresas es una fuente de financiación atractiva porque se devuelve pasado un periodo de tiempo relativamente largo (varios años), por lo que se supone que en ese periodo la empresa ha tenido tiempo de rentabilizar la financiación, aunque tiene como inconveniente que se devuelve más que lo que se recibió, y que se debe pagar todo de una sola vez. Para los inversores también es una fuente atractiva porqué obtienen una rentabilidad fija y ya conocida, aunque no muy elevada
+								</li>
+								<li>
+									¿Quiénes pueden beneficiarse de esta fuente de financiación? Solo las grandes empresas que tienen una buena imagen en el mercado, pues ello facilita los inversores tengan la seguridad que la empresa devolverá el dinero prestado cuando venza la operación. También los países (Estados) recurren a este tipo de fuentes a largo y muy largo plazo, que es lo que se llama deuda pública
+								</li>
 							</ul>
 							
 						</li>
 						<li class="list-group-item">
 							<strong>
-								2.	Crédito comercial  
+								2.	Crédito comercial
 							</strong>
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Esta fuente no nos facilita una cantidad de dinero directamente, sino que permite a la empresa adquirir algo y pagarlo en una fecha futura, por lo que podemos disponer de esos bienes sin haberlos pagado todavía
+								</li>
+								<li>
+									Para poder conseguirlo tendremos que llegar a un acuerdo con los proveedores para poder pagarles en fechas posteriores a las de la compra. Lo normal es que esta operación se concrete por medio de una letra, en la cual se indica quién es la persona que tiene que pagar, y en qué fecha o plazo debe de hacerlo, siendo lo mas normal el pago a 30, 60 y 90 días
+								</li>
+								<li>
+									Lo que la empresa consigue con esto es adquirir las existencias necesarias y poder venderlas antes de que tenga que pagarlas
+								</li>
+								<li>
+									Es una fuente muy ventajosa porqué permite la compra sin pagar cantidad alguna y sin ningún coste adicional
+								</li>
+								<li>
+									Su inconveniente radica en que el plazo de aplazamiento es muy corto y hay que pagarlo todo
+									<br>
+									Decir también que el crédito comercial puede ser un inconveniente cuando es la empresa la que ofrece esta financiación a sus clientes, puesto que es ella la que tarda un tiempo en cobrar algo que vendió hace tiempo
+								</li>
+								<li>
+									Otro tipo de crédito comercial es aquel que en el que el comprador puede adquirir algo y lo paga en cómodos plazos sin recargo alguno, incluso en algunos casos se aplaza el comienzo de pago a un tiempo después de que se adquiera el bien (plazo de carencia)
+								</li>
 							</ul>
 							
 						</li>
@@ -313,9 +337,24 @@
 								3.	Descuento comercial 
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Esta situación es la opuesta a la vista en el apartado anterior, es decir que es la empresa la que vende sus productos y deja que los clientes nos paguen en un futuro por medio de una letra. Ante esta situación la empresa puede actuar de 2 formas
+									<ul>
+										<li>
+											Esperando que pase el tiempo estipulado (30, 60, 90 días) para cobrar la deuda de sus clientes
+										</li>
+										<li>
+											Gestionar el cobro de la deuda antes del plazo, y para ello se acude a una entidad financiera la cual nos adelantará la cantidad indicada en la letra, eso sí, cobrándonos una comisión fija por dicho pago y unos intereses en función del tiempo que hemos cobrado anticipadamente. A esto se le llama descuento de letras
+										</li>
+									</ul>
+								</li>
+								<li>
+									La ventaja de esta fuente radica en que nos permite cobrar anticipadamente las deudas que deberíamos cobrar en un futuro. Su inconveniente son los intereses y las comisiones que se soportan por dicho cobro. Además, en caso de que la entidad financiera no pueda cobrar las letras negociadas ira contra la empresa con la que negoció dichas letras y se lo cobrará a ella cargándola además las correspondientes comisiones
+								</li>
+								<li>
+									<br/>
+									<img class="img-responsive"src="./img/financiacion3.jpg" />
+								</li>
 							</ul>
 							
 						</li>
@@ -324,9 +363,15 @@
 								4.	El factoring   
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Es una fuente similar a la del apartado anterior, aunque en este caso las letras son adquiridas por unas empresas denominadas "de factoring" las cuales asumen el riesgo de que el deudor inicial no pague
+								</li>
+								<li>
+									Como ventaja tendrá el cobro anticipado de las deudas y que no tiene ninguna responsabilidad si el deudor no paga
+								</li>
+								<li>
+									Como inconveniente que paga unos costes y unos intereses considerables, obviamente superiores a los que paga en un descuento comercial
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
@@ -334,9 +379,58 @@
 								5.	El préstamo    
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Es una operación financiera según la cual una entidad financiera entrega una cantidad de dinero a una persona o empresa (prestamista y prestatario), la cual se compromete, mediante contrato, a devolver en un plazo de tiempo y con una periodicidad mensual dicha cantidad prestada más unos intereses
+								</li>
+								<li>
+									La devolución se llevará a cabo pagando una cuota fija de forma mensual, que estará compuesta por la parte del préstamo que se esta devolviendo y los intereses que se pagan. El pago de devolución se comenzará a hacer desde el mismo momento que se formaliza la operación y durará el tiempo que estimen las partes, aunque puede haber un periodo de carencia
+								</li>
+								<li>
+									Cuanto más tiempo fijemos para devolver el préstamo más intereses se pagarán y al revés
+								</li>
+								<li>
+									A la hora de formalizar un préstamo habrá que establecer las siguientes variables
+									<ul>
+										<li>
+											Cantidad de dinero que se solicita.  C
+										</li>
+										<li>
+											Tiempo establecido para devolver el préstamo.  n
+										</li>
+										<li>
+											Tipo de interés.  i
+										</li>
+										<li>
+											Comisión de apertura.
+										</li>
+										<li>
+											Cuota mensual
+										</li>
+										<li>
+											Cantidad que hay que pagar todos los meses.  Ca
+										</li>
+										<li>
+											Principal, capital amortizado...
+										</li>
+										<li>
+											<br/>
+											<img class="img-responsive"src="./img/financiacion5.jpg" />
+										</li>
+									</ul>
+								</li>
+								<li>
+									La ventaja de los préstamos es que se dispone del dinero en el momento inicial y se devuelve poco a poco a lo largo de un periodo de tiempo que puede ser muy largo
+								</li>
+								<li>
+									Su inconveniente es que se pagan unos intereses que suelen ser elevados y al final se puede llegar a devolver bastante más que la cantidad que se solicitó inicialmente
+								</li>
+								<li>
+									Es muy importante distinguir los préstamos hipotecarios de los préstamos al consumo
+									<strong>
+										Los primeros
+									</strong> son los que se otorgan para adquirir una vivienda, mientras que <strong>los segundos</strong> son los que se conceden para adquirir cualquier otro bien (coches, viajes…). La diferencia estriba en que<strong> los primeros tienen un tipo de interés más bajo que los segundos, y un periodo de reembolso más largo</strong>
+									
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
@@ -344,9 +438,18 @@
 								6.	El crédito    
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Es una variante de los préstamos. Aquí la entidad financiera pone a disposición del prestatario una cantidad de dinero en una cuenta, y solo tendrá que devolver lo que ha tomado y pagar los intereses sobre la cantidad de dinero que utilice
+								</li>
+								<li>
+									Una variante de este tipo de producto es el  crédito en póliza, que consiste en que las entidades financieras permiten a sus clientes que dispongan de más dinero del que tienen en sus cuentas, lo que técnicamente se llama tener un descubierto. Posteriormente el cliente tendrá que devolver el descubierto con unos intereses elevados
+								</li>
+								<li>
+									La ventaja de esta fuente es la de disponer de un dinero que no tenemos en cualquier momento
+								</li>
+								<li>
+									Su desventaja, que hay que pagar unos intereses altos, superiores a los de los créditos normales
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
@@ -354,9 +457,9 @@
 								7.	Las tarjetas de crédito   
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Decir de ellas que nos permiten comprar bienes y pagarlos al mes siguiente sin ninguna comisión, lo que nos lleva a afirmar que es un tipo de financiación a muy corto plazo sin coste alguno
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
@@ -364,9 +467,18 @@
 								8.	El Leasing   
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Es una fuente de financiación ideal para aquellos que necesiten una maquinaria, un vehículo u otros medios técnicos
+								</li>
+								<li>
+									Consiste en que una entidad de leasing adquiere un bien determinado, y le alquila a una empresa a cambio del pago de una renta mensual durante el periodo de tiempo que dure la operación. Una vez que se termina dicho periodo la empresa arrendadora podrá ejercer la opción de compra con lo que el bien pasará a ser de su propiedad. El mantenimiento del vehículo o las máquinas lo asume quién disfruta del mismo
+								</li>
+								<li>
+									Esta opción tiene como ventajas el disponer de un bien desde el momento inicial  sin haberlo pagado
+								</li>
+								<li>
+									Como inconveniente estará el alto coste que supone esta fuente, puesto que finalmente se paga una cantidad muy superior al precio inicial de la máquina
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
@@ -374,19 +486,71 @@
 								9.	El Renting    
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Es una variante del leasing que consiste en alquilar cualquier medio técnico de la misma forma que el leasing
+								</li>
+								<li>
+									La diferencia estriba en que es la empresa de renting la que se encarga del mantenimiento de los bienes alquilados, así como de su renovación por modelos mas actualizados. De esta manera la empresa cliente consigue tener siempre los últimos modelos de los diferentes medios técnicos y además con un servicio de mantenimiento incluido
+								</li>
+								<li>
+									Esta sería su principal ventaja junto con las desgravaciones fiscales
+								</li>
+								<li>
+									sus desventajas están relacionadas al elevado coste que le supone a la empresa y que el bien nunca llega a ser de su propiedad
+								</li>
 							</ul>
 						</li>
 						<li class="list-group-item">
 							<strong>
-								10.	Subvenciones   
+								10.	Subvenciones
 							</strong>	
 							<ul>
-								<li></li>
-								<li></li>
-								<li></li>
+								<li>
+									Son cantidades de dinero que la empresa recibe como ayuda para la buena marcha de la empresa
+								</li>
+								<li>
+									Este dinero no hay que devolverlo por lo que se tratará de una fuente no exigible y en la inmensa mayoría de los casos proviene de algún Organismo Público
+								</li>
+								<li>
+									Tipos de subvenciones:
+									<ul>
+										<li>
+											<strong>
+												De establecimiento
+											</strong>
+											<br>
+											Se obtienen cuando se crea una empresa y es necesario cumplir una serie de requisitos referidos a: actividad a realizar, lugar de ubicación del negocio, tipo de personas emprendedoras...
+										</li>
+										<li>
+											<strong>
+												De capital
+											</strong>
+											<br>
+											Para aquellas situaciones en que la marcha económica de la empresa no es buena y se le concede una ayuda para mejorar los resultados económicos y así poder hacer frente a sus obligaciones
+										</li>
+										<li>
+											<strong>
+												De inversión
+											</strong>
+											<br>
+											Son ayudas que se obtienen por adquirir bienes que formen parte del inmovilizado material e inmaterial de la empresa y que sirvan para la mejor marcha de la empresa en un futuro. Ejemplo: compra de maquinaria, de una patente, ordenadores...
+										</li>
+										<li>
+											<strong>
+												De contratación 
+											</strong>
+											<br>
+											Para favorecer la contratación de ciertos colectivos con especial dificultad para encontrar trabajo
+										</li>
+										
+									</ul>
+								</li>
+								<li>
+									La ventaja de esta fuente es el coste cero que le supone a la empresa
+								</li>
+								<li>
+									la desventaja es que es difícil de obtener en muchas ocasiones; que primero hay que pagar lo adquirido y después se da la subvención; que tardan en cobrarse, o que en ciertas actividades no existen
+								</li>
 							</ul>
 						</li>
 					</ul>
