89742dcf3724aef5e658e5248bfbfd0d4323726a
Liferbis <liferbis@gmail.com>
2015-02-09 12:50:38 +0100

    g

---
+++ Agenda vacaciones/informe.php
@@ -0,0 +1,20 @@
+<?php require_once "header.php" ?>
+	<div class="text-center">
+		<h1  >Informe de vacaciones y otros permisos</h1>
+
+<div class="table-responsive">
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+<?php require_once "pie.php" ?>
