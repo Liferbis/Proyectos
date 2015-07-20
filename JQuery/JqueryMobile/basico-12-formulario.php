8d30da9afa460ac4b166bcd74f90944337ffc980
Liferbis <liferbis@gmail.com>
2015-07-20 13:05:37 +0200

    jqm

---
+++ JQuery/JqueryMobile/basico-12-formulario.php
@@ -0,0 +1,11 @@
+<?php
+$nombre=$_POST["name"];
+$email=$_POST["email"];
+$descrip=$_POST["descrip"];
+$sexo=$_POST["sexo"];
+echo "Nombre: ".$nombre."<br>";
+echo "email: ".$email."<br>";
+echo "descrip: ".$descrip."<br>";
+echo "sexo: ".$sexo."<br>";
+
+ ?>
